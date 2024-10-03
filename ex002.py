68c241472bc67e9fa5a66620520faad30b3fde23
Toni <tonictlima@gmail.com>
2024-10-02 23:25:16 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,15 @@
+# Exercício 002 Crie um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
+
+nome = input('Qual o seu nome?')
+print('Bem vindo ao mundo da programação' ,nome)
+print('Bem vindo ao mundo da programação {}!'.format(nome))
+
+# Desafio 002 Crie um script que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada
+
+dia = input('Digite o dia do seu nascimento:')
+mes = input('Digite o mês do seu nascimento:')
+ano = input('Digite o ano do seu nascimento:')
+print('Você nasceu no dia: {}'.format(dia),'de {}'.format(mes),'do ano de: {}'.format(ano))
+
+
+
