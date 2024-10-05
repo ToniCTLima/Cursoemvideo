d51c32907940b0e76cd93a1f61727fd3589a4173
Toni <tonictlima@gmail.com>
2024-10-05 02:23:31 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -1,17 +1,17 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e a raiz quadrada
-#n1 = int(input('Digite um número: '))
-#print('O número é: {}!'.format(n1))
-#print('O dobro desse número é: {}!'.format(n1*2))
-#print('O triplo desse número é: {}!'.format(n1*3))
-#print('A raiz quadrada desse número é: {}!'.format(n1**(1/2)))
+n1 = int(input('Digite um número: '))
+print('O número é: {}!'.format(n1))
+print('O dobro desse número é: {}!'.format(n1*2))
+print('O triplo desse número é: {}!'.format(n1*3))
+print('A raiz quadrada desse número é: {}!'.format(n1**(1/2)))
 
 #outra forma do código
-#n = int(input('Digite um número: '))
-#d = n*2
-#t = n*3
-#r = n**(1/2)
-#print('O dobro de {} vale {}.'.format(n, d))
-#print('O triplo de {} vale {}. A raiz quadrada de {} é igual a {:.2f}'.format(n, t, n, r))
+n = int(input('Digite um número: '))
+d = n*2
+t = n*3
+r = n**(1/2)
+print('O dobro de {} vale {}.'.format(n, d))
+print('O triplo de {} vale {}. A raiz quadrada de {} é igual a {:.2f}'.format(n, t, n, r))
 
 # Usando o f-string para substituir o .format
 
