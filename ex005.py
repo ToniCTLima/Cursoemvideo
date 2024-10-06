a5d78433c9893655278803585e0f5677e346b13c
Toni <tonictlima@gmail.com>
2024-10-05 23:37:31 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia o número inteiro e mostre na tela seu sucessor e seu antecessor
+
+n = int(input('Digite um número: '))
+a = n - 1
+s = n + 1
+print(f'O sucessor de {n} é: {s} e o antecessor de {n} é: {a}')
+
+# Outra forma de fazer o código utlizando uma variável
+
+n = int(input('Digite um número: '))
+print(f'Analisando o valor {n}, seu antecessor é {n-1} e o sucessor é {n+1}')
+        
