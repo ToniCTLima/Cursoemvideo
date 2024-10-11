ccc248831e673f33b0d4ce380c1e5ea6717383dc
Toni <tonictlima@gmail.com>
2024-10-10 21:16:45 -0300

    Add files via upload

---
+++ ex017.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia o comprimento do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa
+
+import math
+from math import hypot
+
+math hypot()
