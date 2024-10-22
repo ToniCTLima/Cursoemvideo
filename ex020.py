7757d61de0327b06c132eab79c60bd859be66919
Toni <tonictlima@gmail.com>
2024-10-22 19:41:41 -0300

    Update ex020.py

--- ex020.py
+++ ex020.py
@@ -22,7 +22,7 @@ import random
 # Solução do professor:
 
 n1 = str(input('Primeiro aluno: '))
-n2 = str(input('segundo aluno: '))
+n2 = str(input('Segundo aluno: '))
 n3 = str(input('Terceiro aluno: '))
 n4 = str(input('Quarto aluno: '))
 lista = [n1, n2, n3, n4]
