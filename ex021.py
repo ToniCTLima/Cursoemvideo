2b2e143efb28f89a5ef48d11434996a243e6371d
Toni <tonictlima@gmail.com>
2024-10-22 20:29:25 -0300

    Update ex021.py

--- ex021.py
+++ ex021.py
@@ -1,4 +1,5 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+# Primeira tentativa com a solução do professor a música não tocava, coloquei o arquivo no CO-PILOT ele adicionou os comando pygame.mixer.music.set_volume(1.0), arquivo tocou por um segundo e parou, CO-PILOT sugeriu colocar o comando while pygame.mixer.music.get_busy():
 
 import pygame
 import time
